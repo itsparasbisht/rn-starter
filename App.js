c37562d1b6f466d533b366fabf987b732c6b2b3a
itsparasbisht <iamparasbisht@gmail.com>
2023-09-06 20:42:52 +0530

    Pressable Component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { View, Button } from "react-native";
+import { View, Image, Pressable } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="Press"
-        onPress={() => console.log("pressed")}
-        color="midnightblue"
-        disabled
-      />
+      <Pressable
+        onPress={() => console.log("press")}
+        onPressIn={() => console.log("press in")}
+        onLongPress={() => console.log("long press")}
+      >
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
     </View>
   );
 }
