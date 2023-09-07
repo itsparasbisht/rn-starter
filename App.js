9190d6ca06958bbff5e3c50da4e0c298e145ac53
itsparasbisht <iamparasbisht@gmail.com>
2023-09-07 19:53:00 +0530

    StatusBar component

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
-import { useState } from "react";
-import { View, Button, Modal, Text } from "react-native";
+import { View, StatusBar } from "react-native";
 
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title="Press" onPress={() => setIsModalVisible(true)} />
-      <Modal
-        visible={isModalVisible}
-        onRequestClose={() => setIsModalVisible(false)} // to trigger close on gestures/back
-        animationType="slide"
-        presentationStyle="pageSheet" // only affects ios
-      >
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text>Hi from Modal!</Text>
-          <Button title="Close" onPress={() => setIsModalVisible(false)} />
-        </View>
-      </Modal>
+      <StatusBar barStyle="dark-content" />
     </View>
   );
 }
