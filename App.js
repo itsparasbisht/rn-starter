1884c21b7a0a39bdc24ae5f7e59c7293d5f4a10c
itsparasbisht <iamparasbisht@gmail.com>
2023-09-07 19:57:50 +0530

    ActivityIndicator component

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { View, StatusBar } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <StatusBar barStyle="dark-content" />
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="large" color="midnightblue" />
+      <ActivityIndicator size="large" color="midnightblue" animating={false} />
     </View>
   );
 }
