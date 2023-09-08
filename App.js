6c52fd93af463b61e66736813fe9f6d945950222
itsparasbisht <iamparasbisht@gmail.com>
2023-09-08 19:43:44 +0530

    StyleSheet api

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
-import { Alert, Button, View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title="Alert" onPress={() => Alert.alert("Invalid Data!")} />
-      <Button
-        title="Alert 2"
-        onPress={() => Alert.alert("Invalid Data!", "DOB Incorrect!")}
-      />
-      <Button
-        title="Alert 3"
-        onPress={() =>
-          Alert.alert("Invalid Data!", "DOB Incorrect", [
-            { text: "Cancel", onPress: () => console.log("cancel pressed") },
-            { text: "OK", onPress: () => console.log("ok pressed") },
-          ])
-        }
-      />
+    <View style={styles.container}>
+      <Text>App</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 60,
+  },
+});
