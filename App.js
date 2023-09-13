669add69d085d7a8e4d4863525f52518e905f1f5
itsparasbisht <iamparasbisht@gmail.com>
2023-09-13 16:59:28 +0530

    PokemonCard component

--- App.js
+++ App.js
@@ -1,42 +1,19 @@
-import { View, StyleSheet, Text, SafeAreaView, Platform } from "react-native";
-import CustomButton from "./components/customButton/CustomButton";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <Text style={styles.text}>Welcome!</Text>
-          <CustomButton title={"Press Me"} onPress={() => alert("Pressed!")} />
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <PokemonCard />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-    backgroundColor: "plum",
-  },
   container: {
     flex: 1,
-    paddingTop: Platform.OS === "android" ? 20 : 0,
-  },
-  box: {
-    padding: 30,
-  },
-  text: {
-    ...Platform.select({
-      ios: {
-        color: "purple",
-      },
-      android: {
-        color: "blue",
-      },
-    }),
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
+    backgroundColor: "#f5f5f5",
+    paddingTop: Platform.OS === "android" ? 15 : 0,
   },
 });
