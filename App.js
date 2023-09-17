be4836b5eb641b69be2f6d7addac8322c19041da
itsparasbisht <iamparasbisht@gmail.com>
2023-09-17 20:16:10 +0530

    TextInput component

--- App.js
+++ App.js
@@ -1,48 +1,23 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  StatusBar,
-  SectionList,
-} from "react-native";
-import pokemonList from "./data.json";
-import groupedPokemonList from "./gruoped-data.json";
+import { useState } from "react";
+import { StatusBar, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+
   return (
     <View style={styles.container}>
       <StatusBar />
-      {/* <FlatList
-        data={pokemonList}
-        renderItem={({ item }) => {
-          return (
-            <View key={item.id} style={styles.cardBox}>
-              <Text style={styles.cardText}>{item.type}</Text>
-              <Text style={styles.cardText}>{item.name}</Text>
-            </View>
-          );
-        }}
-        keyExtractor={(item) => item.id.toString()}
-        ItemSeparatorComponent={<View style={{ height: 10 }} />}
-        ListEmptyComponent={<Text>No items found!</Text>}
-        ListHeaderComponent={<Text>Pokemon list</Text>}
-        ListFooterComponent={<Text>List ends</Text>}
-      /> */}
-
-      <SectionList
-        sections={groupedPokemonList}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.cardBox}>
-              <Text style={styles.cardText}>{item}</Text>
-            </View>
-          );
-        }}
-        renderSectionHeader={({ section }) => (
-          <Text style={styles.sectionHeaderText}>{section.type}</Text>
-        )}
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="email@example.com"
+        autoCorrect={false}
+        autoCapitalize="none"
+        // secureTextEntry // when dealing with passwords
+        // keyboardType="phone-pad"
       />
+      <Text>My name is {name}</Text>
     </View>
   );
 }
@@ -51,21 +26,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
-    paddingHorizontal: 10,
   },
-  cardBox: {
-    backgroundColor: "white",
+  input: {
+    height: 40,
+    margin: 12,
     padding: 10,
-    borderRadius: 8,
-    borderWidth: 2,
-  },
-  cardText: {
-    fontSize: 22,
-    fontWeight: "bold",
-  },
-  sectionHeaderText: {
-    backgroundColor: "white",
-    fontSize: 24,
-    fontWeight: "bold",
+    borderWidth: 1,
   },
 });
